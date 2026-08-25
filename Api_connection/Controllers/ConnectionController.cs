095147fb5d4f1a66b9db65f976734d18e16816fc
Student <Student@LabVM2049939>
2026-08-25 11:23:41 +0200

    Add project files.

---
+++ Api_connection/Controllers/ConnectionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Api_connection.Controllers
+{// start of namespace
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionController : ControllerBase
+    {
+        //Post request method to do the connection to the database  
+        [HttpPost]
+        public string db_connection()
+        { //start of db_connection method
+          //Connection string to connect to the database  
+            string message = "";
+
+            try
+            { //start of try
+                string connection_string = @"Datasource=(localdb)\api_endpoint;database=testing_api,";
+
+                SqlConnection connect = new SqlConnection(connection_string);
+
+                connect.Open();
+                message = "Connection to the database is successful";
+            }// end of try
+            catch(Exception error)
+            { //start of catch
+                message = error.Message;
+            }//end of catch
+            return message;
+        }//end of db_connection method
+    }//end of class
+}//end of namespace
---
+++ Api_connection/Controllers/store_info.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Api_connection.Controllers
+{// start of namespace
+    //post request method to store user information
+    //from fullname, email and password
+    [Route("api/[controller]")]
+    [ApiController]
+    public class store_info 
+    {
+        [HttpPost]
+        public string store_userinfo(string fullname, string email, string password)
+        {//start of method
+
+            // temp message
+            string message = "";
+
+
+            //try and catch
+            try
+            {
+                //connection string
+                string connection_string = @"Datasource=(localdb)\api_endpoint;database=testing_api,";
+
+                //create connection
+                SqlConnection connect = new SqlConnection(connection_string);
+
+                //open connection
+                connect.Open();
+
+                //Query
+                string query = @"INSERT INTO users Values('"+fullname+"','"+email+"','"+password+"');";
+
+                //run query
+                //using SqlCommand to execute the query
+                SqlCommand run_query = new SqlCommand(query, connect);
+
+                //run OR execute the query
+                run_query.ExecuteNonQuery();
+
+                //Return a message
+                message = "User information stored successfully";
+
+                //close connection
+                connect.Close();
+
+            }//end of try
+            catch (Exception error)
+            {
+                //start of catch
+
+
+            }//end of catch
+
+
+
+            return "it works";
+
+
+        }//end of store_userinfo method
+    }//end of class
+}//end of namespace}
